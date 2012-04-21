cc609ae2135520b46785c229bf2b12cbcefc5f3b
Tiago Zaniquelli <ztiago@gmail.com>
2012-04-21 16:03:17 -0300

    tarefa 5. disassembly

--- src/parser.c
+++ src/parser.c
@@ -141,6 +141,9 @@ void parse_options(int argc, char *argv[])
 			case 'f':
 				parse_format(optarg); break;
 
+			case 'D':
+				config.disasm_section = optarg; break;
+
 			default:
 				fprintf(stderr, "pev: try '--help' for more information\n");
 				exit(EXIT_FAILURE);

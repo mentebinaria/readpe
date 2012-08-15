4bf60b29b0555b465b688fcaadc264f97ae56196
Fernando Mercês <fernando@mentebinaria.com.br>
2012-08-15 09:22:16 -0300

    remove uneeded string.h include

--- src/output.c
+++ src/output.c
@@ -19,10 +19,7 @@
 	along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-#include <string.h>
-#include <stdlib.h>
 #include <ctype.h>
-#include <stdio.h>
 #include "output.h"
 #include "common.h"
 

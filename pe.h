9d401177d127446cfb045c932a05b1baf505d0bc
Jardel Weyrich <jweyrich@gmail.com>
2012-11-08 02:55:52 -0200

    libpe: Introduce new function pe_get_data_directory.

--- pe.h
+++ pe.h
@@ -69,22 +69,24 @@ typedef uint64_t QWORD;
 #define RT_TOOLBAR        241  // configuration of toolbars
 
 // directory Entries
-#define IMAGE_DIRECTORY_ENTRY_EXPORT          0   // Export Directory
-#define IMAGE_DIRECTORY_ENTRY_IMPORT          1   // Import Directory
-#define IMAGE_DIRECTORY_ENTRY_RESOURCE        2   // Resource Directory
-#define IMAGE_DIRECTORY_ENTRY_EXCEPTION       3   // Exception Directory
-#define IMAGE_DIRECTORY_ENTRY_SECURITY        4   // Security Directory
-#define IMAGE_DIRECTORY_ENTRY_BASERELOC       5   // Base Relocation Table
-#define IMAGE_DIRECTORY_ENTRY_DEBUG           6   // Debug Directory
-//      IMAGE_DIRECTORY_ENTRY_COPYRIGHT       7   // (X86 usage)
-#define IMAGE_DIRECTORY_ENTRY_ARCHITECTURE    7   // Architecture Specific Data
-#define IMAGE_DIRECTORY_ENTRY_GLOBALPTR       8   // RVA of GP
-#define IMAGE_DIRECTORY_ENTRY_TLS             9   // TLS Directory
-#define IMAGE_DIRECTORY_ENTRY_LOAD_CONFIG    10   // Load Configuration Directory
-#define IMAGE_DIRECTORY_ENTRY_BOUND_IMPORT   11   // Bound Import Directory in headers
-#define IMAGE_DIRECTORY_ENTRY_IAT            12   // Import Address Table
-#define IMAGE_DIRECTORY_ENTRY_DELAY_IMPORT   13   // Delay Load Import Descriptors
-#define IMAGE_DIRECTORY_ENTRY_COM_DESCRIPTOR 14   // COM Runtime descriptor
+typedef enum {
+	IMAGE_DIRECTORY_ENTRY_EXPORT			= 0, // Export Directory
+	IMAGE_DIRECTORY_ENTRY_IMPORT			= 1, // Import Directory
+	IMAGE_DIRECTORY_ENTRY_RESOURCE			= 2, // Resource Directory
+	IMAGE_DIRECTORY_ENTRY_EXCEPTION			= 3, // Exception Directory
+	IMAGE_DIRECTORY_ENTRY_SECURITY			= 4, // Security Directory
+	IMAGE_DIRECTORY_ENTRY_BASERELOC			= 5, // Base Relocation Table
+	IMAGE_DIRECTORY_ENTRY_DEBUG				= 6, // Debug Directory
+	//IMAGE_DIRECTORY_ENTRY_COPYRIGHT			= 7, // (X86 usage)
+	IMAGE_DIRECTORY_ENTRY_ARCHITECTURE		= 7, // Architecture Specific Data
+	IMAGE_DIRECTORY_ENTRY_GLOBALPTR			= 8, // RVA of GP
+	IMAGE_DIRECTORY_ENTRY_TLS				= 9, // TLS Directory
+	IMAGE_DIRECTORY_ENTRY_LOAD_CONFIG		= 10, // Load Configuration Directory
+	IMAGE_DIRECTORY_ENTRY_BOUND_IMPORT		= 11, // Bound Import Directory in headers
+	IMAGE_DIRECTORY_ENTRY_IAT				= 12, // Import Address Table
+	IMAGE_DIRECTORY_ENTRY_DELAY_IMPORT		= 13, // Delay Load Import Descriptors
+	IMAGE_DIRECTORY_ENTRY_COM_DESCRIPTOR	= 14  // COM Runtime descriptor
+} ImageDirectoryEntry;
 
 #pragma pack(push, 1)
 
@@ -429,6 +431,7 @@ bool pe_get_dos(PE_FILE *pe, IMAGE_DOS_HEADER *header);
 //bool pe_get_tls_callbacks(PE_FILE *pe);
 bool pe_get_resource_directory(PE_FILE *pe, IMAGE_RESOURCE_DIRECTORY *dir);
 bool pe_get_resource_entries(PE_FILE *pe);
+IMAGE_DATA_DIRECTORY *pe_get_data_directory(PE_FILE *pe, ImageDirectoryEntry entry);
 
 IMAGE_SECTION_HEADER* pe_rva2section(PE_FILE *pe, QWORD rva);
 

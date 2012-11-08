26ee25ecacf93e60c5960e09b50937986f5dffd2
Jardel Weyrich <jweyrich@gmail.com>
2012-11-08 18:49:45 -0200

    libpe: Add very basic support for the security directory.

---
+++ types.h
@@ -0,0 +1,13 @@
+#ifndef LIBPE_TYPES_H
+#define LIBPE_TYPES_H
+
+#include <stdbool.h>
+#include <inttypes.h>
+
+typedef uint32_t DWORD;
+typedef int32_t LONG;
+typedef uint8_t BYTE;
+typedef uint16_t WORD;
+typedef uint64_t QWORD;
+
+#endif

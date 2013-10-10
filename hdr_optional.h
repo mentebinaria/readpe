b4f7f30732abb1180584890c71b6eb983c50b972
Jardel Weyrich <jweyrich@gmail.com>
2013-10-10 14:43:53 -0300

    Add 2 missing subsystems, inspired by 76ea354. Thanks to Dmitry Mostovenko.

--- hdr_optional.h
+++ hdr_optional.h
@@ -23,28 +23,32 @@
 #include <inttypes.h>
 
 typedef enum {
-	// An unknown subsystem
-	IMAGE_SUBSYSTEM_UNKNOWN					= 0,
-	// Device drivers and native Windows processes
-	IMAGE_SUBSYSTEM_NATIVE					= 1,
-	// The Windows graphical user interface (GUI) subsystem
-	IMAGE_SUBSYSTEM_WINDOWS_GUI				= 2,
-	// The Windows character subsystem
-	IMAGE_SUBSYSTEM_WINDOWS_CUI				= 3,
-	// The Posix character subsystem
-	IMAGE_SUBSYSTEM_POSIX_CUI				= 7,
-	// Windows CE
-	IMAGE_SUBSYSTEM_WINDOWS_CE_GUI			= 9,
-	// An Extensible Firmware Interface (EFI) application
-	IMAGE_SUBSYSTEM_EFI_APPLICATION			= 10,
-	// An EFI driver with boot services
-	IMAGE_SUBSYSTEM_EFI_BOOT_SERVICE_DRIVER = 11,
-	// An EFI driver with run-time services
-	IMAGE_SUBSYSTEM_EFI_RUNTIME_DRIVER		= 12,
-	// An EFI ROM image
-	IMAGE_SUBSYSTEM_EFI_ROM					= 13,
-	// XBOX
-	IMAGE_SUBSYSTEM_XBOX					= 14
+	// Unknown subsystem
+	IMAGE_SUBSYSTEM_UNKNOWN						= 0,
+	// No subsystem required (device drivers and native system processes)
+	IMAGE_SUBSYSTEM_NATIVE						= 1,
+	// Windows graphical user interface (GUI) subsystem
+	IMAGE_SUBSYSTEM_WINDOWS_GUI					= 2,
+	// Windows character-mode user interface (CUI) subsystem
+	IMAGE_SUBSYSTEM_WINDOWS_CUI					= 3,
+	// OS/2 CUI subsystem
+	IMAGE_SUBSYSTEM_OS2_CUI						= 5,
+	// POSIX CUI subsystem
+	IMAGE_SUBSYSTEM_POSIX_CUI					= 7,
+	// Windows CE system
+	IMAGE_SUBSYSTEM_WINDOWS_CE_GUI				= 9,
+	// Extensible Firmware Interface (EFI) application
+	IMAGE_SUBSYSTEM_EFI_APPLICATION				= 10,
+	// EFI driver with boot services
+	IMAGE_SUBSYSTEM_EFI_BOOT_SERVICE_DRIVER 	= 11,
+	// EFI driver with run-time services
+	IMAGE_SUBSYSTEM_EFI_RUNTIME_DRIVER			= 12,
+	// EFI ROM image
+	IMAGE_SUBSYSTEM_EFI_ROM						= 13,
+	// Xbox system
+	IMAGE_SUBSYSTEM_XBOX						= 14,
+	// Boot application.
+	IMAGE_SUBSYSTEM_WINDOWS_BOOT_APPLICATION	= 16
 } WindowsSubsystem;
 
 typedef enum {

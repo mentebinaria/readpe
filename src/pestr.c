2198859bebce0a1140373200389a930143df5e23
Fernando Mercês <fernando@mentebinaria.com.br>
2013-11-27 00:00:50 -0200

    line buffer size increased; added country domains to list

--- src/pestr.c
+++ src/pestr.c
@@ -28,7 +28,7 @@
 #define PROGRAM "pestr"
 #define BUFSIZE 4
 #define OVECCOUNT 30
-#define LINE_BUFFER 2048
+#define LINE_BUFFER 32768
 
 typedef struct {
 	unsigned short strsize;
@@ -153,11 +153,30 @@ static bool ishostname(const char *s, const encoding_t encoding)
 		"^[a-zA-Z]{3,}://.*$", // protocol://
 		"[1-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}:?" // ipv4
 	};
+
 	const char *domains[] = {
-		".aero",	".asia",	".biz",	".com",	".cat",	".com",	".coop",
-		".info",	".int",	".jobs",	".mobi",	".museum",	".name",	".net", ".br",
-		".org",	".pro",	".tel",	".travel",	".xxx", ".edu", ".gov", ".mil",
-		".jus",	
+".xxx", ".asia", ".jobs", ".mobi", ".travel", ".xxx",
+".aero", ".arpa", ".biz", ".com", ".coop", ".edu", ".gov", ".info", ".int", ".jus", ".mil",
+".museum", ".name", ".net", ".org", ".pro", ".ac", ".ad", ".ae", ".af", ".ag", 
+".ai", ".al", ".am", ".an", ".ao", ".aq", ".ar", ".as", ".at", ".au", ".aw", ".az", ".ba",
+".bb", ".bd", ".be", ".bf", ".bg", ".bh", ".bi", ".bj", ".bm", ".bn", ".bo", ".br", ".bs",
+".bt", ".bv", ".bw", ".by", ".bz", ".ca", ".cc", ".cd", ".cf", ".cg", ".ch", ".ci", ".ck",
+".cl", ".cm", ".cn", ".co", ".cr", ".cu", ".cv", ".cx", ".cy", ".cz", ".de", ".dj", ".dk",
+".dm", ".do", ".dz", ".ec", ".ee", ".eg", ".er", ".es", ".et", ".eu", ".fi", ".fj", ".fk",
+".fm", ".fo", ".fr", ".ga", ".gb", ".gd", ".ge", ".gf", ".gg", ".gh", ".gi", ".gl", ".gm",
+".gn", ".gp", ".gq", ".gr", ".gs", ".gt", ".gu", ".gw", ".gy", ".hk", ".hm", ".hn", ".hr",
+".ht", ".hu", ".id", ".ie", ".il", ".im", ".in", ".io", ".iq", ".ir", ".is", ".it", ".je",
+".jm", ".jo", ".jp", ".ke", ".kg", ".kh", ".ki", ".km", ".kn", ".kr", ".kw", ".ky", ".kz",
+".la", ".lb", ".lc", ".li", ".lk", ".lr", ".ls", ".lt", ".lu", ".lv", ".ly", ".ma", ".mc",
+".md", ".me", ".mg", ".mh", ".mk", ".ml", ".mm", ".mn", ".mo", ".mp", ".mq", ".mr", ".ms",
+".mt", ".mu", ".mv", ".mw", ".mx", ".my", ".mz", ".nb", ".nc", ".ne", ".nf", ".ng", ".ni",
+".nl", ".no", ".np", ".nr", ".nu", ".nz", ".om", ".pa", ".pe", ".pf", ".pg", ".ph", ".pk",
+".pl", ".pm", ".pn", ".pr", ".ps", ".pt", ".pw", ".py", ".qa", ".re", ".ro", ".ru", ".rw",
+".sa", ".sb", ".sc", ".sd", ".se", ".sg", ".sh", ".si", ".sj", ".sk", ".sl", ".sm", ".sn",
+".so", ".sr", ".ss", ".st", ".su", ".sv", ".sy", ".sz", ".tc", ".td", ".tf", ".tg", ".th",
+".tj", ".tk", ".tl", ".tm", ".tn", ".to", ".tr", ".tt", ".tv", ".tw", ".tz", ".ua", ".ug",
+".uk", ".um", ".us", ".uy", ".uz", ".va", ".vc", ".ve", ".vg", ".vi", ".vn", ".vu", ".wf",
+".ws", ".ye", ".yt", ".yu", ".za", ".zm", ".zw"
 	};
 
 	if (!isalnum((int) *s))

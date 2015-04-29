2d60a60524596c88873a9605d3f818a00e77a231
Fernando Mercês <nandu88@gmail.com>
2015-04-29 00:41:27 -0300

    license changed to LGPLv3

--- README.md
+++ README.md
@@ -5,3 +5,4 @@ The PE library
 
 [![Build Status](https://travis-ci.org/merces/libpe.png)](https://travis-ci.org/merces/libpe)
 
+[![LGPL](http://www.gnu.org/graphics/lgplv3-147x51.png)](http://www.gnu.org/licenses/lgpl.html)
--- pe.c
+++ pe.c
@@ -1,20 +1,18 @@
 /*
-	libpe - the PE library
+    This file is part of libpe.
 
-	Copyright (C) 2010 - 2013 libpe authors
+    libpe is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
 
-	This program is free software: you can redistribute it and/or modify
-	it under the terms of the GNU General Public License as published by
-	the Free Software Foundation, either version 3 of the License, or
-	(at your option) any later version.
+    libpe is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
 
-	This program is distributed in the hope that it will be useful,
-	but WITHOUT ANY WARRANTY; without even the implied warranty of
-	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-	GNU General Public License for more details.
-
-	You should have received a copy of the GNU General Public License
-	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+    You should have received a copy of the GNU Lesser General Public License
+    along with libpe.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 #include "pe.h"

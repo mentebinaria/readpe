fafd297ea34e222f8618b7dbc4be3dc181d69c15
boddumanohar <201451024@iiitvadodara.ac.in>
2017-06-26 11:59:08 +0530

    add sections hash

--- fuzzy.c
+++ fuzzy.c
@@ -474,10 +474,8 @@ char *calc_hash(const char *alg_name, const unsigned char *data, size_t size, ch
     fuzzy_hash_buf(data, size, output);
     return output;
   }
-
   const EVP_MD *md = EVP_get_digestbyname(alg_name);
   //assert(md != NULL); // We already checked this in parse_hash_algorithm()
-
   unsigned char md_value[EVP_MAX_MD_SIZE];
   unsigned int md_len;
 
@@ -494,15 +492,14 @@ char *calc_hash(const char *alg_name, const unsigned char *data, size_t size, ch
   EVP_DigestInit_ex(md_ctx, md, NULL);
   EVP_DigestUpdate(md_ctx, data, size);
   EVP_DigestFinal_ex(md_ctx, md_value, &md_len);
-
 #if OPENSSL_VERSION_NUMBER < 0x10100000L
   EVP_MD_CTX_cleanup(md_ctx);
 #else
   EVP_MD_CTX_free(md_ctx);
 #endif
-
   for (unsigned int i=0; i < md_len; i++)
     sprintf(&output[i * 2], "%02x", md_value[i]);
+  EVP_cleanup();
 
 return output;
 }
@@ -511,13 +508,14 @@ hash_ get_hashes(char *name,const unsigned char *data, size_t data_size) {
   hash_ sample;
   
   int MD_SIZE = EVP_MAX_MD_SIZE * 2 + 1;
-  char hash_value[EVP_MAX_MD_SIZE * 2 + 1];
+  char hash_value[MD_SIZE];
   sample.name  = name;                             // TODO : allow memory dynamically.
+//sample.name = (char *)malloc(
   sample.md5 = (char *)malloc(MD_SIZE*sizeof(char *));
   sample.sha1 = (char *)malloc(MD_SIZE*sizeof(char *));
   sample.sha256 = (char *)malloc(MD_SIZE*sizeof(char *));
   sample.ssdeep = (char *)malloc(MD_SIZE*sizeof(char *));
-
+ 
   memcpy(sample.md5, calc_hash("md5", data, data_size, hash_value), MD_SIZE * sizeof(char *));
   memcpy(sample.sha1, calc_hash("sha1", data, data_size, hash_value), MD_SIZE * sizeof(char *));
   memcpy(sample.sha256, calc_hash("sha256", data, data_size, hash_value), MD_SIZE * sizeof(char *));
@@ -557,8 +555,8 @@ hash_ get_headers_optional_hash(pe_ctx_t *ctx) {
 hdr_ get_headers_hash(pe_ctx_t *ctx) {
   
   hash_ dos = get_headers_dos_hash(ctx);
-  hash_ coff = get_headers_coff_hash(ctx);
   hash_ optional = get_headers_optional_hash(ctx);
+  hash_ coff = get_headers_coff_hash(ctx);
   
   hdr_ sample_hdr;
   sample_hdr.dos = dos;
@@ -568,47 +566,31 @@ hdr_ get_headers_hash(pe_ctx_t *ctx) {
   return sample_hdr;
 }
  
-/*
-coff_hdr get_coff_hash(pe_ctx_t *ctx) {
-  coff_hdr coff;
+section_ get_sections_hash(pe_ctx_t *ctx) {
+  int c = pe_sections_count(ctx);
+  hash_ sample[c];
+  const unsigned char *data = NULL;
+  uint64_t data_size = 0;
+  char *name;
+  IMAGE_SECTION_HEADER ** const sections = pe_sections(ctx);
+  for (int i=0; i<c; i++) {
+            data_size = sections[i]->SizeOfRawData;
+            data = LIBPE_PTR_ADD(ctx->map_addr, sections[i]->PointerToRawData);
 
-  const IMAGE_COFF_HEADER *coff_hdr = pe_coff(ctx);
-    const unsigned char *data = (const unsigned char *)coff_hdr;
-    uint64_t data_size = sizeof(IMAGE_COFF_HEADER);
-  char hash_value[EVP_MAX_MD_SIZE * 2 + 1];
+            if (!pe_can_read(ctx, data, data_size)) {
+               //EXIT_ERROR("Unable to read section data");
+		fprintf(stderr, "%s\n", "unable to read sections data");
+		exit(1);
+            }
 
-  coff.name  = "IMAGE_COFF_HEADER";
-  coff.md5 = calc_hash("md5", data, data_size, hash_value);
-  coff.sha1 = calc_hash("sha1", data, data_size, hash_value);
-  coff.md5 = calc_hash("sha256", data, data_size, hash_value);
-  coff.ssdeep = calc_hash("ssdeep", data, data_size, hash_value);
-  return coff;
+            if (data_size) {
+		name = (char *)sections[i]->Name;
+                sample[i] =  get_hashes(name, data, data_size);
+            }
+        }
+  section_ sample_sect;
+  sample_sect.sections = (hash_ *)malloc( c * sizeof(hash_));
+  sample_sect.sections = sample;
+  sample_sect.count = c;
+  return sample_sect;
 }
-
-optional_hdr get_optional_hash(pe_ctx_t *ctx) {
-  optional_hdr optional;
-  const IMAGE_OPTIONAL_HEADER *opt_hdr = pe_optional(ctx);
-  const unsigned char *data = (const unsigned char *)opt_hdr;  // TODO : revert to opt_hdr->_64 to support both opt_hdr->_32
-  uint64_t data_size = sizeof(IMAGE_OPTIONAL_HEADER_64);
-
-  char hash_value[EVP_MAX_MD_SIZE * 2 + 1];
-  // optional.name  = "IMAGE_OPTIONAL_HEADER";
-  optional.md5 = calc_hash("md5", data,data_size, hash_value );
-  optional.sha1 = calc_hash("sha1", data,data_size, hash_value );
-  optional.md5 = calc_hash("sha256", data,data_size, hash_value );
-  optional.ssdeep = calc_hash("ssdeep", data,data_size, hash_value);
-  return optional;
-} 
-*/
-/*j
-pehash_ get_all_hashes(pe_ctx_t *ctx) {
-  
-  hdr_ 
-  hasheslist.dos = get_dos_hash(ctx);
-  hasheslist.coff = get_coff_hash(ctx);
-  hasheslist.optional = get_optional_hash(ctx);
-  return hasheslist;
-
-}
-*/
-
--- fuzzy.h
+++ fuzzy.h
@@ -96,12 +96,24 @@ typedef struct {
 	hash_ optional;
 }hdr_;
 
+typedef struct {
+	int count;
+	hash_ *sections;
+}section_;
+
+// General functions
 char *calc_hash(const char *alg_name, const unsigned char *data, size_t size, char *output);
+
+// Functions to get hash of headers
 hash_ get_hashes(char *name,const unsigned char *data, size_t size);
 hash_ get_headers_dos_hash(pe_ctx_t *ctx);
 hash_ get_headers_coff_hash(pe_ctx_t *ctx);
 hash_ get_headers_optional_hash(pe_ctx_t *ctx);
 hdr_ get_headers_hash(pe_ctx_t *ctx);
+
+// Functions to get hash of sections
+section_ get_sections_hash(pe_ctx_t *ctx);
+
 
 /**
  * @brief Construct a fuzzy_state object and return it.

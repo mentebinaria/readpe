acc1dbb8ab858981fd0de78e5c63978fee14097e
Jardel Weyrich <jweyrich@gmail.com>
2019-07-31 18:32:58 -0300

    peldd: libpe already parses all imports for us.

--- src/peldd.c
+++ src/peldd.c
@@ -93,65 +93,15 @@ static void parse_options(int argc, char *argv[])
 
 }
 
-
-static void print_imports(pe_ctx_t *ctx)
+static void print_dependencies(pe_ctx_t *ctx)
 {
-	const IMAGE_DATA_DIRECTORY *dir = pe_directory_by_entry(ctx, IMAGE_DIRECTORY_ENTRY_IMPORT);
-	if (dir == NULL)
-		EXIT_ERROR("import directory not found")
-
-	const uint64_t va = dir->VirtualAddress;
-	if (va == 0) {
-		fprintf(stderr, "import directory not found\n");
-		return;
+	output_open_scope("Dependencies", OUTPUT_SCOPE_TYPE_ARRAY);
+	const pe_imports_t *imports = pe_imports(ctx);
+	for (size_t i=0; i < imports->dll_count; i++) {
+		const pe_imported_dll_t *dll = &imports->dlls[i];
+		output(dll->name, NULL);
 	}
-	uint64_t ofs = pe_rva2ofs(ctx, va);
-
-	while (1) {
-		IMAGE_IMPORT_DESCRIPTOR *id = LIBPE_PTR_ADD(ctx->map_addr, ofs);
-		if (!pe_can_read(ctx, id, sizeof(IMAGE_IMPORT_DESCRIPTOR))) {
-			// TODO: Should we report something?
-			output_close_scope();
-			return;
-		}
-
-		if (!id->u1.OriginalFirstThunk && !id->FirstThunk)
-			break;
-
-		ofs += sizeof(IMAGE_IMPORT_DESCRIPTOR);
-		const uint64_t aux = ofs; // Store current ofs
-
-		ofs = pe_rva2ofs(ctx, id->Name);
-		if (ofs == 0)
-			break;
-
-		const char *dll_name_ptr = LIBPE_PTR_ADD(ctx->map_addr, ofs);
-		// Validate whether it's ok to access at least 1 byte after dll_name_ptr.
-		// It might be '\0', for example.
-		if (!pe_can_read(ctx, dll_name_ptr, 1)) {
-			// TODO: Should we report something?
-			break;
-		}
-
-		char dll_name[MAX_DLL_NAME];
-		strncpy(dll_name, dll_name_ptr, sizeof(dll_name)-1);
-		// Because `strncpy` does not guarantee to NUL terminate the string itself, this must be done explicitly.
-		dll_name[sizeof(dll_name) - 1] = '\0';
-
-		//output_open_scope("Library", OUTPUT_SCOPE_TYPE_OBJECT);
-		output("Name", dll_name);
-
-		ofs = pe_rva2ofs(ctx, id->u1.OriginalFirstThunk ? id->u1.OriginalFirstThunk : id->FirstThunk);
-		if (ofs == 0) {
-			output_close_scope(); // Library
-			break;
-		}
-
-		ofs = aux; // Restore previous ofs
-
-		//output_close_scope(); // Library
-	}
-
+	output_close_scope();
 }
 
 int main(int argc, char *argv[])
@@ -188,14 +138,10 @@ int main(int argc, char *argv[])
 	output_open_document();
 
 	IMAGE_DATA_DIRECTORY **directories = pe_directories(&ctx);
+	if (directories == NULL)
+		EXIT_ERROR("directories not found");
 
-	// imports
-		if (directories != NULL)
-			print_imports(&ctx);
-		else {
-			fprintf(stderr, "directories not found\n");
-		}
-	
+	print_dependencies(&ctx);
 
 	output_close_document();
 

080460385880c7e3c75fcb38333414e62b5c5aa2
Đoàn Trần Công Danh <congdanhqx@gmail.com>
2020-06-07 23:31:17 +0700

    repack: allocate 1 more byte for NUL character
    strncpy(3) needs 1 more byte for NUL character.
    
    gcc barfs on current code-base:
    
    > warning: '__builtin_strncpy' specified bound 80 equals destination size [-Wstringop-truncation]
    >  106 |   return __builtin___strncpy_chk (__dest, __src, __len, __bos (__dest));

--- src/pepack.c
+++ src/pepack.c
@@ -288,14 +288,14 @@ int main(int argc, char *argv[])
 	if (!loaddb(&dbfile, options))
 		fprintf(stderr, "WARNING: without valid database file, %s will search in generic mode only\n", PROGRAM);
 
-	char value[MAX_MSG];
+	char value[MAX_MSG + 1];
 
 	// TODO(jweyrich): Create a new API to retrieve map_addr.
 	// TODO(jweyrich): Should we use `LIBPE_PTR_ADD(ctx->map_addr, ep_offset)` instead?
 	const unsigned char *pe_data = ctx.map_addr;
 
 	// packer by signature
-	if (compare_signature(pe_data, ep_offset, dbfile, value, sizeof(value)))
+	if (compare_signature(pe_data, ep_offset, dbfile, value, sizeof(value) - 1))
 		;
 	// generic detection
 	else if (generic_packer(&ctx, ep_offset))

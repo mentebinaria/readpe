7257d35391569899cb94178dd31d844f846b1002
Đoàn Trần Công Danh <congdanhqx@gmail.com>
2020-06-07 23:35:03 +0700

    hashes: allocate 16 more bytes for the buffer
    We'll print a long long unsigned int later in MAGIC_PE64.
    Which will, theoretically, yields up to 20 bytes in some platforms.
    Hence, gcc will report a warning for that snprintf.
    
    In practice, we will never run into that situation,
    but 16 bytes is not that expensive.
    
    Let's play an easy game.

--- hashes.c
+++ hashes.c
@@ -366,7 +366,7 @@ static void imphash_load_imported_functions(pe_ctx_t *ctx, uint64_t offset, char
 
 	uint64_t ofs = offset;
 
-	char hint_str[16];
+	char hint_str[32];
 	char fname[MAX_FUNCTION_NAME];
 	bool is_ordinal = false;
 
